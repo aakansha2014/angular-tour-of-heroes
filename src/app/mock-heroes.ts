b7140f2a9e65584992564b73d4b397e444c23386
Akansha <aakansha2014@gmail.com>
2018-09-01 14:50:43 -0400

    Created selectable list of Cats

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Paws' },
+  { id: 2, name: 'Tabby' },
+  { id: 3, name: 'Tomcat' },
+  { id: 4, name: 'Celery' },
+  { id: 5, name: 'Magneta' },
+  { id: 6, name: 'Blue' },
+  { id: 7, name: 'Periwinkle' },
+  { id: 8, name: 'Garfield' },
+  { id: 9, name: 'Odi' },
+  { id: 10, name: 'Tornado' }
+];
